cc9275f63e94b4609b14acb3651c55b18c5fd69f
giorgio1985 <g.adonoo@yahoo.it>
2018-05-12 00:55:17 +0000

    A simple file using node.js and mongo db noSql database to output Json values

--- mongoDbFile.js
+++ mongoDbFile.js
@@ -1,33 +1,31 @@
-function myFunction(va){  
-//giorgio1985 
+function foo(ob){  
 var http=require('http');
-var proto=8000;
 http.createServer(function(req, res){
-	res.writeHead(200,{'content-type':'text/plain'});
-	res.end('Connected to the port number :' +proto+' end the value of the object conteined are : '+va);
-}).listen(proto);
-console.log('hey up!');
+	res.writeHead(200, {'content-type':'text/plain'});
+	res.end('hello mongo...'+ob);
+}).listen(5000);
+console.log('hell boy..');
 }
 
-var mongo=require('mongodb').MongoClient;
-var urlMongodb='mongodb://127.0.0.1:27017/';
-var dbMongo='database';
-var collections='collection_001';
-var oggetto=[
-{nome:'Giorgio', cognome:'Agrippino', eta:32, citta:'Milano'},
-{nome:'Carlo', cognome:'Fatebenefratelli', eta:66, citta:'Mantova'},
-{nome:'Pierangela',cognome:'Venturelli', eta:62, citta:'Brescia'},
-{nome:'Alessandro', cognome:'Volta', eta:25, citta:'Padova'},
-{nome:'Franco', cognome:'Rossini', eta:55, citta:'Genova'}
-];
-mongo.connect(urlMongodb, function(err1,resp){
-if (err1) throw err1;
-var dbconn=resp.db(dbMongo);
-dbconn.collection(collections).insertMany(oggetto, function(err3, ok){
-if (err3) throw err3;
-var jsonString=JSON.stringify(oggetto);
-console.log('Vista dei valori presenti nell oggetto: '+jsonString);
-myFunction(jsonString);
-});
-resp.close();
+var mongoo=require('mongodb').MongoClient;
+var mongoodb='namedb';
+var mongoohost='mongodb://127.0.0.1:27017/';
+var mongoocollection='collectiondb';
+var oggetto={nome:'Giorgio',cognome:'Asterix', anni:32, citta:'Milano'};
+
+mongoo.connect(mongoohost, function(err,ris){
+	if (err) throw err;
+	var Db=ris.db(mongoodb);
+	Db.collection(mongoocollection).insertOne(oggetto, function(err, res){
+if (err) throw err;
+var obj=JSON.stringify(oggetto);
+console.log('Dati inseriti sono');
+foo(obj);
+	});
+	Db.collection(mongoocollection).find({}).toArray(function(err,data){
+		if (err) throw err;
+		console.log(data);
+	});
+
+	ris.close();
 });
