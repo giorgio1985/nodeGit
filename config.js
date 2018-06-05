cccecf6ca85c7d86d847972c5f345ff22bd28750
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2018-06-05 19:56:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports={
-	"database" : "mongodb://mongo*:internazionale*@ds245680.mlab.com:45680/database001",
-	"ports" : 4000,
-	"secretKey":"secretKey"
+	'port':8000,
+	'time':new Date(),
+	'db': 'mongodb://mongo1985:internazionale1985@ds245680.mlab.com:45680/database001'
+
+
+
+
 }
